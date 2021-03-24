e1f8f68c7e9f9a17e7af61032e1f85804efffe06
Lucky <lavanya.pedapati@gmail.com>
2021-03-24 19:19:57 +0000

    Any operation using if

--- Training/Calculator/index.js
+++ Training/Calculator/index.js
@@ -1,12 +1,28 @@
 const readline = require('readline-sync');
 console.log('Welcome to the calculator!');
-//TO MULTIPLY TWO NUMBERS AND PRINT OUT THE RESULT.
-console.log('Please enter some input:');
-const response1 = readline.prompt();
-console.log('Please enter the second input:');
-const response2 = readline.prompt();
-console.log(+response1 * +response2); 
-/*
-(response1 * response2) will also work as strings are 
-converted to numeric for all arithmetic operations except '+'.
-*/
+console.log('==========================');
+/* LET'S ENHANCE OUR CALCULATOR TO SUPPORT ALL ARITHMETIC
+ OPERATORS i,e., +, -, * AND / USING IF CONSTRUCT. */
+
+ console.log('Please enter the operator:');
+ const response = readline.prompt();
+
+ console.log('Please enter the first number:');
+ const response1 = readline.prompt();
+ console.log('Please enter the second number:');
+ const response2 = readline.prompt();
+ let answer;
+
+ if (response == '+') {
+     answer = +response1 + +response2;
+     console.log('The answer is: ' + answer);
+ } else if (response == '-') {
+     answer = +response1 - +response2;
+     console.log('The answer is: ' + answer);
+ } else if (response == '*') {
+     answer = +response1 * +response2;
+     console.log('The answer is: ' + answer);
+ } else if (response == '/') {
+     answer = +response1 / +response2;
+     console.log('The answer is: ' + answer);
+ }
