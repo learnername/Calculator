45cf3d2efb9552d3bd6dadf7e7915e668969146d
Lucky <lavanya.pedapati@gmail.com>
2021-03-24 21:43:08 +0000

    Functions and loops

--- Training/Calculator/index.js
+++ Training/Calculator/index.js
@@ -1,45 +1,76 @@
 const readline = require('readline-sync');
-console.log('Welcome to the calculator!');
-console.log('==========================');
-/* ENHANCEMENT TO THE CALCULATOR TO PERFORM OPERATION ON ANY 
-NUMBER OF NUMBERS. */
-
-console.log('Please enter the operator:');
-const response = readline.prompt();
-console.log('How many numbers do you want to ' + response + '?');
-const numbers = readline.prompt();
-let operands = [];
-operands.length = numbers;
-for (let i = 0; i < operands.length; i++){
- console.log('please enter the number ' + (i+1) + ':');
- operands[i] = readline.prompt();  
+function printWelcomeMessage() {
+	console.log('Welcome to the calculator');
+	console.log('=========================');
 }
 
-let answer;
-switch (response){
-    case '+':
-        answer = 0;
-        for (let i=0; i < operands.length; i++){
-            answer = answer + +operands[i];
-        }
-        break;
-    case '-':
-        answer = operands[0];
-        for (let i=1; i<operands.length; i++){
-            answer = answer - operands[i];
-        }
-        break;
-    case '*':
-        answer = 1;
-        for (let i=0; i<operands.length; i++){
-            answer = answer * operands[i];
-        }
-        break;
-    case '/':
-        answer = operands[0];
-        for (let i=1; i<operands.length; i++){
-            answer = answer/operands[i];
-        }
-        break;
+function checkValidInput(inputvalue){
+    let a = +inputvalue;
+    if (isNaN(a)){
+        console.log('Please enter a valid number');
+        const inputagain = readline.prompt();
+        a = checkValidInput(inputagain);
+    } else {
+        return a;
+    } return a;
 }
-console.log('The answer is: ' + answer);
+
+function enterInput(operator) {
+    console.log('How many numbers do you want to ' + operator + '?');
+    const numbers = readline.prompt();
+	let operands = [];
+	operands.length = +numbers;
+	let i = 0;
+	while (i < operands.length){
+	    console.log('please enter the number ' + (i+1) + ':');
+		const enteredvalue = readline.prompt(); 
+		operands[i] = checkValidInput(enteredvalue);
+		i++;
+		}
+		return operands;
+	}
+
+function doCalculate(operator,operands){
+    let answer;
+        switch (operator){
+        	case '+':
+        	    answer = 0;
+        	    for (let i=0; i < operands.length; i++){
+        	        answer = answer + operands[i];
+        	    }
+        	    break;
+        	case '-':
+        	    answer = operands[0];
+        	    for (let i=1; i<operands.length; i++){
+        	        answer = answer - operands[i];
+        	    }
+        	    break;
+        	case '*':
+        	    answer = 1;
+        	    for (let i=0; i<operands.length; i++){
+        	        answer = answer * operands[i];
+        	    }
+        	    break;
+        	case '/':
+        	    answer = operands[0];
+        	    for (let i=1; i<operands.length; i++){
+        	        answer = answer/operands[i];
+        	    }
+        	    break;
+        	}
+    return answer;
+}
+
+function performOneCalculation() {
+	console.log('Please enter the operator: ');
+	const response = readline.prompt();
+	const x = enterInput(response);
+	const y = doCalculate(response,x);
+	console.log('The answer is: ' + y);
+	//performOneCalculation();
+}
+		
+printWelcomeMessage();
+while(true){
+	performOneCalculation();
+}
