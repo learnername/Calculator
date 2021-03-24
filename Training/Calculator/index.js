7305cb890491269d14d9b6e0da2f6615cabf021c
Lucky <lavanya.pedapati@gmail.com>
2021-03-24 19:30:09 +0000

    operate on any number of numbers

--- Training/Calculator/index.js
+++ Training/Calculator/index.js
@@ -1,33 +1,45 @@
 const readline = require('readline-sync');
 console.log('Welcome to the calculator!');
 console.log('==========================');
-/* LET'S ENHANCE OUR CALCULATOR TO SUPPORT ALL ARITHMETIC
- OPERATORS i,e., +, -, * AND / USING SWITCH. */
+/* ENHANCEMENT TO THE CALCULATOR TO PERFORM OPERATION ON ANY 
+NUMBER OF NUMBERS. */
 
- console.log('Please enter the operator:');
- const response = readline.prompt();
+console.log('Please enter the operator:');
+const response = readline.prompt();
+console.log('How many numbers do you want to ' + response + '?');
+const numbers = readline.prompt();
+let operands = [];
+operands.length = numbers;
+for (let i = 0; i < operands.length; i++){
+ console.log('please enter the number ' + (i+1) + ':');
+ operands[i] = readline.prompt();  
+}
 
- console.log('Please enter the first number:');
- const response1 = readline.prompt();
- console.log('Please enter the second number:');
- const response2 = readline.prompt();
- let answer;
-
- switch (response) {
+let answer;
+switch (response){
     case '+':
-       answer = +response1 + +response2;
-        console.log('The answer is: ' + answer);
+        answer = 0;
+        for (let i=0; i < operands.length; i++){
+            answer = answer + +operands[i];
+        }
         break;
     case '-':
-       answer = +response1 - +response2;
-        console.log('The answer is: ' + answer); 
+        answer = operands[0];
+        for (let i=1; i<operands.length; i++){
+            answer = answer - operands[i];
+        }
         break;
     case '*':
-       answer = +response1 * +response2;
-        console.log('The answer is: ' + answer);
+        answer = 1;
+        for (let i=0; i<operands.length; i++){
+            answer = answer * operands[i];
+        }
         break;
     case '/':
-       answer = +response1 / +response2;
-        console.log('The answer is: ' + answer);
+        answer = operands[0];
+        for (let i=1; i<operands.length; i++){
+            answer = answer/operands[i];
+        }
         break;
-};
+}
+console.log('The answer is: ' + answer);
